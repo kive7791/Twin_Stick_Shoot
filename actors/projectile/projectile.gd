0693780f127253ee86e64f3e937992a2490f41bc
kive7791 <kvel1097@gmail.edu>
2024-09-12 14:39:32 -0600

    Tile Map

--- actors/projectile/projectile.gd
+++ actors/projectile/projectile.gd
@@ -3,13 +3,13 @@ extends Area2D
 var velocity: Vector2 = Vector2(0,0)
 
 func fire(forward: Vector2, speed: float):
-    velocity = forward * speed
-    look_at(position + forward)
+	velocity = forward * speed
+	look_at(position + forward)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-    position += velocity * delta
+	position += velocity * delta
 
 
 func _on_time_to_live_timeout():
-    queue_free()
+	queue_free()
