2e130062d1ad63175361ea44fc021f12e2dcf75f
Kira <89668329+kive7791@users.noreply.github.com>
2024-12-14 11:37:54 -0700

    File not required

--- SharedScripts/hud.gd
+++
@@ -1,7 +0,0 @@
-extends CanvasLayer
-
-func _ready() -> void:
-    ScoreManager.score_updated.connect(_update_score)
-
-func _update_score(new_score: int):
-    $Label.text = "Score: " + str(new_score)
